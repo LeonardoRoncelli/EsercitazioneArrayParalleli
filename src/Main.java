54d5b28547cb46ca52e7260de50d206bcb9022af
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-09-16 12:54:49 +0200

    Aggiunta menù.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input=new Scanner (System.in);
-        int num;
-        System.out.print("Inserisci il numero di auto");
-        num=input.nextInt();
-        int[]auto=new int[num];
-
+        int num=0;
+        int scelta;
+        String[]marca=new String[num];
+        String[]modello=new String[num];
+        double[]prezzo=new double[num];
+        do {
+            System.out.println("Inserisci 0 per uscire");
+            System.out.println("Inserisci 1 per aggiungere una nuova auto");
+            System.out.println("Inserisci 2 per visualizzare tutte le auto");
+            System.out.println("Inserisci 3 per ricercare l'auto tramite marca e modello");
+            System.out.println("Inserisci 4 per cancellare un'auto");
+            System.out.println("Inserisci 5 per modificare i dati di un auto");
+            System.out.println("Inserisci 6 per visualizzare le auto in ordine crescente");
+            System.out.println("Inserisci 7 per visualizzare tutti i modelli diesel");
+            System.out.print("Inserisci la tua scelta ");
+            scelta=input.nextInt();
+            for (int i = 0; i < num; i++) {
+                System.out.println("Inserisci la marca dell'auto ");
+                marca[i] = input.nextLine();
+                System.out.println("Inserisci il modello dell'auto ");
+                modello[i] = input.nextLine();
+                System.out.println("Inserisci il prezzo dell'auto ");
+                prezzo[i] = input.nextDouble();
+            }
+        }while(scelta!=0);
     }
 }
