d2a887eaaec6c6f6676e57462276d8b32d745948
RONCELLI LEONARDO <RONCELLI.23346>
2024-09-16 12:32:50 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        int num;
+        System.out.print("Inserisci il numero di auto");
+        num=input.nextInt();
+        int[]auto=new int[num];
+
+    }
+}
