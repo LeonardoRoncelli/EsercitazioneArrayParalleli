77851b39096048f37b177534556479f4c55cd3bb
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-09-23 12:27:00 +0200

    Creazione della classe Funzioni, aggiunta delle funzioni nella nuova classe e modifiche delle chiamate delle funzioni nel Main.

---
+++ src/Funzioni.java
@@ -0,0 +1,53 @@
+public class Funzioni {
+    public static void aggiunta (int conta, String[] marca, String[]modello, double[]prezzo, String marcaIns, String modelloIns, double prezzoIns) {
+        marca[conta]=marcaIns;
+        modello[conta]=modelloIns;
+        prezzo[conta]=prezzoIns;
+    }
+    public static void visualizza (String[]marca,String[]modello,double[]prezzo,int conta){
+        for (int i=0;i<conta;i++) {
+            System.out.println(marca[i]);
+            System.out.print(modello[i]);
+            System.out.print(prezzo[i]);
+        }
+    }
+    public static void ricerca (String marca3, String modello3, String[]marca,String[]modello,int conta){
+        for(int i=0;i<conta;i++){
+            if(marca[i].equalsIgnoreCase(marca3)&&modello[i].equalsIgnoreCase(modello3)){
+                System.out.println(marca[i]);
+                System.out.println(modello[i]);
+                break;
+            }
+        }
+    }
+    public static void cancella (String cancMarca,String cancModello,String temp,int conta,String[]marca,String[]modello){
+        for (int i = 0; i < conta; i++) {
+            if (marca[i].equalsIgnoreCase(cancMarca) && modello[i].equalsIgnoreCase(cancModello)) {
+                temp = marca[i];
+                marca[i] = marca[conta];
+                marca[conta] = temp;
+                conta--;
+                break;
+            }
+        }
+    }
+    public static void ordinate (int conta,String[]marca,int min){
+        for(int i=0;i<conta-1;i++){
+            min=i;
+            for(int j=i+1;j<conta;j++){
+                if(marca[j].compareTo(marca[min])<0){
+                    min=j;
+                }
+            }
+        }
+    }
+    public static void diesel (int conta,String[] modello,String[]marca,double[]prezzo){
+        for(int i=0;i<conta;i++){
+            if(modello[i].equalsIgnoreCase("diesel")){
+                System.out.println(marca[i]);
+                System.out.println(modello[i]);
+                System.out.println(prezzo[i]);
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -27,13 +27,13 @@ public class Main {
                         String modelloIns = input.next();
                         System.out.print("Inserisci il prezzo dell'auto: ");
                         double prezzoIns = input.nextDouble();
-                        aggiunta(conta,marca,modello,prezzo, marcaIns,modelloIns,prezzoIns);
+                        Funzioni.aggiunta(conta,marca,modello,prezzo, marcaIns,modelloIns,prezzoIns);
                     } else {
                         System.out.println("Lunghezza massima array raggiunta");
                     }
                 }
                 case 2:{
-                    visualizza(marca,modello,prezzo,conta);
+                    Funzioni.visualizza(marca,modello,prezzo,conta);
                 }
                 case 3:{
                     String marca3;
@@ -42,7 +42,7 @@ public class Main {
                     marca3=input.next();
                     System.out.print("Inserisci il modello da ricercare: ");
                     modello3=input.next();
-                    ricerca(marca3,modello3,marca,modello,conta);
+                    Funzioni.ricerca(marca3,modello3,marca,modello,conta);
                 }
                 case 4: {
                     String cancMarca;
@@ -52,7 +52,7 @@ public class Main {
                     cancMarca = input.next();
                     System.out.print("Inserisci il modello da cancellare: ");
                     cancModello = input.next();
-                    cancella(cancMarca,cancModello,temp,conta,marca,modello);
+                    Funzioni.cancella(cancMarca,cancModello,temp,conta,marca,modello);
                 }
                 case 5:{
                     String ricercaMarca;
@@ -74,63 +74,12 @@ public class Main {
                 }
                 case 6:{
                     int min=0;
-                    ordinate(conta,marca,min);
+                    Funzioni.ordinate(conta,marca,min);
                 }
                 case 7:{
-                    diesel(conta,modello,marca,prezzo);
+                    Funzioni.diesel(conta,modello,marca,prezzo);
                 }
             }
         }while(scelta!=0);
-    }
-    private static void aggiunta (int conta, String[] marca, String[]modello, double[]prezzo, String marcaIns, String modelloIns, double prezzoIns) {
-        marca[conta]=marcaIns;
-        modello[conta]=modelloIns;
-        prezzo[conta]=prezzoIns;
-    }
-    private static void visualizza (String[]marca,String[]modello,double[]prezzo,int conta){
-        for (int i=0;i<conta;i++) {
-            System.out.println(marca[i]);
-            System.out.print(modello[i]);
-            System.out.print(prezzo[i]);
-        }
-    }
-    private static void ricerca (String marca3, String modello3, String[]marca,String[]modello,int conta){
-        for(int i=0;i<conta;i++){
-            if(marca[i].equalsIgnoreCase(marca3)&&modello[i].equalsIgnoreCase(modello3)){
-                System.out.println(marca[i]);
-                System.out.println(modello[i]);
-                break;
-            }
-        }
-    }
-    private static void cancella (String cancMarca,String cancModello,String temp,int conta,String[]marca,String[]modello){
-        for (int i = 0; i < conta; i++) {
-            if (marca[i].equalsIgnoreCase(cancMarca) && modello[i].equalsIgnoreCase(cancModello)) {
-                temp = marca[i];
-                marca[i] = marca[conta];
-                marca[conta] = temp;
-                conta--;
-                break;
-            }
-        }
-    }
-    private static void ordinate (int conta,String[]marca,int min){
-        for(int i=0;i<conta-1;i++){
-            min=i;
-            for(int j=i+1;j<conta;j++){
-                if(marca[j].compareTo(marca[min])<0){
-                    min=j;
-                }
-            }
-        }
-    }
-    private static void diesel (int conta,String[] modello,String[]marca,double[]prezzo){
-        for(int i=0;i<conta;i++){
-            if(modello[i].equalsIgnoreCase("diesel")){
-                System.out.println(marca[i]);
-                System.out.println(modello[i]);
-                System.out.println(prezzo[i]);
-            }
-        }
     }
 }
